27161373fb715b3f364a0f767cfa7eb28feb13cb
tempgoodman <i_am_pong@hotmail.com>
2017-07-24 12:34:10 +0800

    replication script

---
+++ SQL_replication_script.sql
@@ -0,0 +1,24 @@
+SELECT compatibility_level  
+FROM sys.databases WHERE name = 'employee';  
+GO  
+
+ALTER DATABASE employee 
+SET COMPATIBILITY_LEVEL = 110;  
+GO  
+
+
+EXEC master.dbo.sp_addlinkedserver @server = N'loopback', @srvproduct=N'',
+ @provider=N'SQLNCLI', @datasrc=@@SERVERNAME
+
+ EXEC master.dbo.sp_serveroption @server=N'loopback', 
+@optname=N'data access', @optvalue=N'true'
+
+select @@SERVERNAME
+
+select SERVERPROPERTY(N'servername')
+
+ sp_dropserver 'LDTECHDB01\MSSQLSERVER2' 
+ go 
+
+sp_addserver 'LDTECHDB01\MSSQLSERVER2', 'local'
+
